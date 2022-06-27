11f4558774c5d266d36f5b2575e1b08887e91f2d
David Krasnitsky <dikaveman@gmail.com>
2022-06-28 00:02:20 +0300

    Set the stage for working with Tera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::{fs, path::PathBuf};
+
+lazy_static! {
+    static ref TEMPLATES_DIR: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("templates");
+}
+
+fn load_template(name: &str) -> String {
+    let mut template_path = TEMPLATES_DIR.join(name);
+    template_path.set_extension("html");
+    println!("Loading `{}`", template_path.to_string_lossy());
+    fs::read_to_string(template_path).expect("Unable to load template contents")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let template = "test";
+    let template_contents = load_template(template);
+    println!("{template_contents}")
 }
