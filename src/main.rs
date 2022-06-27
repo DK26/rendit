0e86854857193c364c6e612c2acc5188c9cf0a8b
David Krasnitsky <dikaveman@gmail.com>
2022-06-28 01:38:42 +0300

    Implemented the loading of Context from JSON

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate lazy_static;
 
 use std::{fs, path::PathBuf};
 
+use tera::{Context, Tera};
+
 lazy_static! {
     static ref TEMPLATES_DIR: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("templates");
 }
@@ -11,11 +13,31 @@ fn load_template(name: &str) -> String {
     let mut template_path = TEMPLATES_DIR.join(name);
     template_path.set_extension("html");
     println!("Loading `{}`", template_path.to_string_lossy());
-    fs::read_to_string(template_path).expect("Unable to load template contents")
+    fs::read_to_string(template_path).expect("Unable to load raw template.")
+}
+
+fn load_context(name: &str) -> Context {
+    let mut template_path = TEMPLATES_DIR.join(name);
+    template_path.set_extension("json");
+
+    let context_json = fs::read_to_string(template_path).expect("Unable to load template context.");
+
+    Context::from_value(context_json.parse().expect("Unable to parse context JSON."))
+        .expect("Unable to create context from JSON.")
 }
 
 fn main() {
     let template = "test";
     let template_contents = load_template(template);
-    println!("{template_contents}")
+    // println!("{template_contents}");
+
+    // let mut context = Context::new();
+    // context.insert("user", &"D.K");
+
+    let context = load_context(template);
+
+    let result =
+        Tera::one_off(&template_contents, &context, true).expect("Unable to render template.");
+
+    println!("{result}");
 }
