430192e9c52d1f0525cdf5e3aea79a313644f456
David Krasnitsky <dikaveman@gmail.com>
2022-06-30 21:15:24 +0300

    Added support for file based engine + Scan engine type in file + Support for Handlebars engine

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
 use clap::Parser;
+use handlebars::Handlebars;
+use regex::Regex;
 use std::{
     fs::{self, OpenOptions},
     io::{BufWriter, Write},
     path::{Path, PathBuf},
+    str::FromStr,
 };
 use tera::{Context, Tera};
 
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
-struct Cli {
-    #[clap(value_parser)]
-    template_file: PathBuf,
+enum Template {
+    Tera(String),
+    Handlebars(String),
+    Liquid(String),
+    Unknown(String),
+    Raw(String),
 }
 
-// #[macro_use]
-// extern crate lazy_static;
+enum TemplateEngine {
+    Tera,
+    Handlebars,
+    Unknown,
+    NotSpecified,
+}
 
-// #[cfg(build = "debug")]
-// lazy_static! {
-//     static ref TEMPLATES_DIR: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("templates");
+// impl FromStr for TemplateEngine {
+//     type Err = ();
+
+//     fn from_str(template_str: &str) -> Result<Self, Self::Err> {
+//         Ok(match template_str {
+//             "tera" => Template::Tera(String::new()),
+//             "handlebars" | "hbs" => Template::Handlebars(String::new()),
+//             _ => Template::Unknown(String::new()),
+//         })
+//     }
 // }
 
-fn load_context<P: AsRef<Path>>(path: P) -> Context {
-    let context_json = fs::read_to_string(path).expect("Unable to load template context.");
-
-    Context::from_value(context_json.parse().expect("Unable to parse context JSON."))
-        .expect("Unable to create context from JSON.")
+#[derive(Parser)]
+#[clap(
+    author = env!("CARGO_PKG_AUTHORS"),
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    long_about = None
+)]
+struct Cli {
+    /// A template file path, requiring a JSON file of the same name for context.
+    /// e.g. `my_template.html` should have a context file named `my_template.ctx.json` in the same directory.
+    #[clap(value_parser)]
+    template_file: PathBuf,
 }
 
 fn output_render<P: AsRef<Path>>(content: &str, path: P) {
@@ -40,13 +62,41 @@ fn output_render<P: AsRef<Path>>(content: &str, path: P) {
         .expect("Unable to write rendered HTML");
 }
 
-fn load_template<P: AsRef<Path>>(path: P) -> TemplateEngine {
-    let template_contents = fs::read_to_string(path).expect("Unable to load raw template.");
-    TemplateEngine::Tera(template_contents)
-}
+fn load_template<P: AsRef<Path>>(path: P) -> Template {
+    let template_contents = fs::read_to_string(&path).expect("Unable to load raw template.");
 
-enum TemplateEngine {
-    Tera(String),
+    // if let Some(stem) = path.as_ref().file_stem() {
+    //     let stem = &*stem.to_string_lossy();
+    //     println!("Stem: {stem}");
+    // }
+
+    if let Some(extension) = path.as_ref().extension() {
+        let extension = &*extension.to_string_lossy();
+
+        // println!("Extension: {extension}");
+
+        match extension {
+            "tera" => return Template::Tera(template_contents),
+            "hbs" => return Template::Handlebars(template_contents),
+            "liq" => return Template::Liquid(template_contents),
+            _ => {}
+        };
+    }
+
+    // TODO: Decode which type is the content and return it
+
+    let re = Regex::new(r#"^<!--TEMPLATE\s+(?P<engine>\w+)-->"#).expect("Bad regex pattern.");
+
+    let mut re_caps = re.captures_iter(&template_contents);
+
+    if let Some(cap) = re_caps.next() {
+        let engine = &cap["engine"];
+        println!("Template Engine: {engine}");
+        // TODO: Remove engine comment from final product before passing
+        Template::Tera(template_contents)
+    } else {
+        Template::Raw(template_contents)
+    }
 }
 
 fn main() {
@@ -56,11 +106,29 @@ fn main() {
 
     let args = Cli::parse();
 
-    let template_context_file = args.template_file.with_extension("json");
-    let rendered_output_file = args.template_file.with_extension("rendered.html");
+    let template_extension = &*args
+        .template_file
+        .extension()
+        .expect("Template has no file extension.")
+        .to_string_lossy();
 
+    let template_extension = "rendered.".to_string() + template_extension;
+
+    let template_context_file = args.template_file.with_extension("ctx.json");
+
+    let context_json =
+        fs::read_to_string(&template_context_file).expect("Unable to load template context.");
+
+    let context: serde_json::Value =
+        serde_json::from_str(&context_json).expect("Unable to parse context JSON.");
+
+    let rendered_output_file = args.template_file.with_extension(&template_extension);
+
+    // #[cfg(build = "debug")]
     println!("Rendering File: {}", args.template_file.to_string_lossy());
+    // #[cfg(build = "debug")]
     println!("Context File: {}", template_context_file.to_string_lossy());
+    // #[cfg(build = "debug")]
     println!(
         "Rendered Output File: {}",
         rendered_output_file.to_string_lossy()
@@ -69,10 +137,21 @@ fn main() {
     let template_contents = load_template(args.template_file);
 
     let result = match template_contents {
-        TemplateEngine::Tera(contents) => {
-            Tera::one_off(&contents, &load_context(template_context_file), true)
+        Template::Tera(contents) => {
+            let context =
+                Context::from_value(context).expect("Unable to create context from JSON.");
+
+            Tera::one_off(&contents, &context, true).expect("Unable to render template.")
+        }
+        Template::Handlebars(contents) => {
+            let handlebars = Handlebars::new();
+            handlebars
+                .render_template(&contents, &context)
                 .expect("Unable to render template.")
         }
+        Template::Liquid(contents) => todo!(),
+        Template::Unknown(_) => panic!("Unknown template engine."),
+        Template::Raw(raw) => raw,
     };
 
     output_render(&result, rendered_output_file);
